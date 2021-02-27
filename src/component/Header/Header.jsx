3253d117e1d9d051b63d35696d0c5d21cd9bad83
Azu0925 <yamaneko564783@gmail.com>
2021-02-27 21:51:14 +0900

    ヘッダー画像追加

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import {makeStyles} from '@material-ui/styles';
-import {HeaderMenuArea} from './index'
+import {HeaderMenuArea, HeaderLogoArea} from './index';
 
 const useStyles = makeStyles({
+    headerColor: {
+        backgroundColor: '#ffa64d'
+    },
+    contents: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
     tool:{
-        display:'flex',
-        justifyContent:'flex-end',
+        display:'flex'
     }
 })
 
@@ -17,12 +23,14 @@ const Header = () => {
     const IsSignedIn = true;//selectorで参照してサインインしてるかどうか。今は仮でtrue
 
         return (
-            <AppBar>
-                <Toolbar className={classes.tool}>
-
-                    {IsSignedIn && (
-                        <HeaderMenuArea />
-                    )}
+            <AppBar className={classes.headerColor}>
+                <Toolbar className={classes.contents}>
+                    <HeaderLogoArea className={classes.logo} />
+                    <div className={classes.tool}>
+                        {IsSignedIn && (
+                            <HeaderMenuArea />
+                        )}
+                    </div>
                 </Toolbar>
             </AppBar>
         )
